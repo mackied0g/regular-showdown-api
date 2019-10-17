775ff177e5358f9c673160f631ca664177164fc9
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-10-17 17:09:45 -0400

    TIL one can use has_one through and it migrates

--- app/models/showdown.rb
+++ app/models/showdown.rb
@@ -1,4 +1,13 @@
 class Showdown < ApplicationRecord
     has_one :fighter1
     has_one :fighter2
+
+    has_many :move1s, through: :fighter1
+    has_many :move2s, through: :fighter2
+
+    has_many :block1s, through: :fighter1
+    has_many :block2s, through: :fighter2
+
+    has_one :specialmove1, through: :fighter1
+    has_one :specialmove2, through: :fighter2
 end
